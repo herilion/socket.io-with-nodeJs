cf42f8759ad36dac4e8884df50a0837b88e0b4ff
herilion <heritierlionge@yahoo.com>
2022-12-29 14:23:49 +0200

    update external file

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const socket = io();
+const btnSend = document.querySelector('.btnSend')
 const send = () => {
     const text = document.querySelector(".messageInput").value;
     socket.emit('chat message', text);
 };
+btnSend.addEventListener('click', send());
+
 const receive = (msg) => {
     const li = document.createElement('li');
     li.innerText = msg;
