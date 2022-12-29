7840b0c0e6a07c3244af7191ad2cc6107050956d
herilion <heritierlionge@yahoo.com>
2022-12-29 12:50:17 +0200

    initialisation du sockect

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const io = require('socket.io')(http);
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html')
 })
+
+io.on('connection', function (socket) {
+    console.log('un utilisateur est connecté')
+})
 http.listen(8000, function () {
     console.log('server tourne au port 8000');
 })
